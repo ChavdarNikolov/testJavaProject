9b98cfc53454f5d2bd35103199527fe2a383c57f
chavdarnikolov1065 <chavdarnikolov1065@gmail.com>
2021-01-30 21:44:26 +0200

    Added separate class for oranges

--- AAFirstProjectForGitHub/src/mainPackage/MainMethod.java
+++ AAFirstProjectForGitHub/src/mainPackage/MainMethod.java
@@ -1,5 +1,6 @@
 package mainPackage;
 import methodPackage.Methods;
+import methodPackage.Methods1;
 
 //Remember to save(ctrl + s) after making changes
 //a mess
@@ -7,8 +8,9 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		Methods obj = new Methods();
+		Methods1 obj1 = new Methods1();
 		obj.prompt();
-		System.out.println("Oranges");
+		obj1.prompt();
 	}
 	
 }
---
+++ AAFirstProjectForGitHub/src/methodPackage/Methods1.java
@@ -0,0 +1,8 @@
+package methodPackage;
+
+public class Methods1 {
+	public void prompt() {
+		String a = "Oranges";
+		System.out.println(a);
+	}
+}
